a88c939166db6292266f3300253b3c94e2ca2ca5
Bob <Bob@MSI>
2024-01-23 10:48:43 +0800

    作業1

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -14,13 +14,23 @@ namespace WebApplication120240122.Controllers
 
 		public IActionResult Index()
 		{
-			return View();
+			return Content("<h2>Content,你好</h2>","text/plain",System.Text.Encoding.UTF8);
 		}
 		public IActionResult Cities()
 		{
 			var cities=_dbContext.Addresses.Select(c => c.City).Distinct();
 			return Json(cities);
 
+		}
+		public IActionResult Avatar(int id = 1)
+		{
+			Member? member = _dbContext.Members.Find(id);
+			if(member!= null)
+			{
+				byte[]img = member.FileData;
+				return File(img, "image/jpeg");
+			}
+			return NotFound();
 		}
 	}
 }

4acbb51d8beaae26f3020c1a99c7601a2af95ea7
Bob <Bob@MSI>
2024-01-23 09:05:14 +0800

    加入專案檔案。

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication120240122.Models;
+
+namespace WebApplication120240122.Controllers
+{
+	public class ApiController : Controller
+	{
+		private readonly MyDBContext _dbContext;
+		public ApiController(MyDBContext dbContext)
+		{
+			
+			_dbContext = dbContext;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Cities()
+		{
+			var cities=_dbContext.Addresses.Select(c => c.City).Distinct();
+			return Json(cities);
+
+		}
+	}
+}
---
+++ Controllers/HomeWorkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication120240122.Controllers
+{
+    public class HomeWorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
